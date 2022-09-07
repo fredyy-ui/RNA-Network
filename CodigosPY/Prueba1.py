5d243eb087ad1f8acbc1a29ab8d2d59776e548c6
Alfredo RV <fredo.rv02@gmail.com>
2022-09-07 15:20:13 -0500

    Codigo para entrenar la Red Neuronal que clasifica numeros del 0 al 9. Se implemento la class Network y el modulo Mnist_Loader

---
+++ CodigosPY/Prueba1.py
@@ -0,0 +1,29 @@
+import Mnist_Loader
+import Network
+import pickle
+
+training_data, validation_data , test_data = Mnist_Loader.load_data_wrapper()
+
+training_data = list(training_data)
+test_data = list(test_data)
+
+net=Network.Network([784,50,10])
+
+net.SGD( training_data, 30, 10, 3, test_data=test_data)
+
+archivo = open("red_prueba.pkl",'wb')
+pickle.dump(net,archivo)
+archivo.close()
+exit()
+
+#leer el archivo
+
+archivo_lectura = open("red_prueba1.pkl",'rb')
+net = pickle.load(archivo_lectura)
+archivo_lectura.close()
+
+net.SGD( training_data, 10, 10, 3.0, test_data=test_data)
+
+#esquema de como usar la red :
+#imagen = leer_imagen("disco.jpg")
+#print(net.feedforward(imagen))
